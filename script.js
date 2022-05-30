6fdf0ce665b7b14295ca7766f02c8be5a823d5bd
Sergey Agafonov <seriywolf@gmail.com>
2022-05-30 17:58:35 +0700

    Change everything

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 let container = document.querySelector("#container");
+let n = 16;
 let quantity = 256;
 const createCards = () => {
   for (let i = 1; i <= quantity; i++) {
     let card = document.createElement("div");
-    card.id = "card";
+    let cardSize = (960 / n).toString() + "px";
+    card.classList.add(".card");
+    card.style.width = cardSize;
+    card.style.height = cardSize;
+    card.style.border = "1px solid grey";
     container.appendChild(card);
   }
 };
+
 createCards();
 
 let button = document.querySelector("button");
 button.onclick = function () {
-  let size = prompt("1-100", "16");
-  var first = container.firstElementChild;
-  while (first) {
-    first.remove();
-    first = container.firstElementChild;
-  }
-  quantity = size * size;
-  //document.querySelector("#card").remove();
-
-  createCards();
+  n = prompt("1-100", "16");
+  if (n !== null && n<100) {
+    container.innerHTML = "";
+    quantity = n * n;
+    createCards();
+  };
 };
 
 container.addEventListener("mouseover", function (event) {
   if (container === event.target) return;
   // highlight the mouseover target
   event.target.style.backgroundColor = "orange";
-});
-
-container.addEventListener("mouseout", function (event) {
-  // unhighlight the mouseover target
-  event.target.style.backgroundColor = "white";
 });
